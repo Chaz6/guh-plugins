7be3f20893d9040b68736604e57f27767058579c
Simon Stürz <simon.stuerz@guh.io>
2017-10-04 10:44:39 +0200

    fix debian packaging

--- keba/translations/de_DE.ts
+++ keba/translations/de_DE.ts
@@ -5,120 +5,140 @@
     <name>KebaKeContact</name>
     <message>
         <location filename="../plugininfo.h" line="51"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="51"/>
         <source>Keba KeContact</source>
         <extracomment>The name of the plugin Keba KeContact (9142b09f-30a9-43d0-9ede-2f8debe075ac)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="54"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="54"/>
         <source>Keba</source>
         <extracomment>The name of the vendor (f7cda40b-829a-4675-abaa-485697430f5f)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="57"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="57"/>
         <source>Keba KeContact P30</source>
         <extracomment>The name of the DeviceClass (900dacec-cae7-4a37-95ba-501846368ea2)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="60"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="60"/>
         <source>IP Address</source>
         <extracomment>The name of the paramType (730cd3d3-5f0e-4028-a8c2-ced7574f13f3) of Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="63"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="63"/>
         <source>Activity changed</source>
         <extracomment>The name of the autocreated EventType (539e5602-6dd9-465d-9705-3bb59bcf8982)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="66"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="66"/>
         <source>Activity</source>
         <extracomment>The name of the ParamType of StateType (539e5602-6dd9-465d-9705-3bb59bcf8982) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="69"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="69"/>
         <source>Plug State changed</source>
         <extracomment>The name of the autocreated EventType (3b4d29f3-3101-47ad-90fd-269b6348783b)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="72"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="72"/>
         <source>Plug State</source>
         <extracomment>The name of the ParamType of StateType (3b4d29f3-3101-47ad-90fd-269b6348783b) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="75"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="75"/>
         <source>Current changed</source>
         <extracomment>The name of the autocreated EventType (a29c1748-fe97-4830-a56e-e1cc4e618385)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="78"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="78"/>
         <source>Current</source>
         <extracomment>The name of the ParamType of StateType (a29c1748-fe97-4830-a56e-e1cc4e618385) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="81"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="81"/>
         <source>Maximal Current changed</source>
         <extracomment>The name of the autocreated EventType (593656f0-babf-4308-8767-68f34e10fb15)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="84"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="84"/>
         <source>maximal Current</source>
         <extracomment>The name of the ParamType of StateType (593656f0-babf-4308-8767-68f34e10fb15) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="87"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="87"/>
         <source>Set maximal current</source>
         <extracomment>The name of the autocreated ActionType (593656f0-babf-4308-8767-68f34e10fb15)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="90"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="90"/>
         <source>Power changed</source>
         <extracomment>The name of the autocreated EventType (e8f069ca-7fa7-4568-8d4c-165f6d048720)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="93"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="93"/>
         <source>Power</source>
         <extracomment>The name of the ParamType of StateType (e8f069ca-7fa7-4568-8d4c-165f6d048720) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="96"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="96"/>
         <source>Output Enable changed</source>
         <extracomment>The name of the autocreated EventType (0cd5396a-bc41-4c8f-b037-db10991a76c7)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="99"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="99"/>
         <source>Output</source>
         <extracomment>The name of the ParamType of StateType (0cd5396a-bc41-4c8f-b037-db10991a76c7) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="102"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="102"/>
         <source>Set Output</source>
         <extracomment>The name of the autocreated ActionType (0cd5396a-bc41-4c8f-b037-db10991a76c7)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="105"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="105"/>
         <source>Device Reachable changed</source>
         <extracomment>The name of the autocreated EventType (b1a574a6-46b6-44ea-a0bb-9b4de3198967)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="108"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/keba/plugininfo.h" line="108"/>
         <source>reachable</source>
         <extracomment>The name of the ParamType of StateType (b1a574a6-46b6-44ea-a0bb-9b4de3198967) of DeviceClass Keba KeContact P30</extracomment>
         <translation type="unfinished"></translation>
