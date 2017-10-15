a42dc03f5d23ece7bc1972650529495d379de90f
Bernhard Trinnes <bernhard.trinnes@guh.io>
2017-10-15 12:50:36 +0200

    added UDP output and changed Vendor to UDP commander

--- udpcommander/translations/en_US.ts
+++ udpcommander/translations/en_US.ts
@@ -4,36 +4,72 @@
 <context>
     <name>UdpCommander</name>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="30"/>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="36"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="37"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="40"/>
         <source>UDP Commander</source>
-        <extracomment>The name of the plugin UDP Commander (24a8474c-1d86-499e-a76e-9cbfbf48dd72)
+        <extracomment>The name of the plugin UDP Commander (c9a86acf-7b51-452d-874a-7d24dd87033f)
 ----------
-The name of the DeviceClass (6ecd5a8d-595a-4520-85e3-dcc9679edf66)</extracomment>
+The name of the vendor (3434153f-6eb7-4f5c-9045-6d0a952b2596)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="33"/>
-        <source>guh</source>
-        <extracomment>The name of the vendor (2062d64d-3232-433c-88bc-0d33c0ba2ba6)</extracomment>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="43"/>
+        <source>UDP Input</source>
+        <extracomment>The name of the DeviceClass (6ecd5a8d-595a-4520-85e3-dcc9679edf66)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="39"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="46"/>
         <source>port</source>
-        <extracomment>The name of the paramType (1843adcb-e377-44d1-8d70-ab4f9eeb32ec) of UDP Commander</extracomment>
+        <extracomment>The name of the paramType (1843adcb-e377-44d1-8d70-ab4f9eeb32ec) of UDP Input</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="42"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="49"/>
         <source>command</source>
-        <extracomment>The name of the paramType (d0f29961-1624-4b91-a0e8-9b1cc86c44c7) of UDP Commander</extracomment>
+        <extracomment>The name of the paramType (d0f29961-1624-4b91-a0e8-9b1cc86c44c7) of UDP Input</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="45"/>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="52"/>
+        <source>input data changed</source>
+        <extracomment>The name of the autocreated EventType (065a1a0a-d324-4ae6-a461-bef8143e8795)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="55"/>
+        <source>Input Data</source>
+        <extracomment>The name of the ParamType of StateType (065a1a0a-d324-4ae6-a461-bef8143e8795) of DeviceClass UDP Input</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="58"/>
         <source>command received</source>
-        <extracomment>The name of the EventType 5fecbba3-ffbb-456b-872c-a2f571c681cb of deviceClass UDP Commander</extracomment>
+        <extracomment>The name of the EventType 5fecbba3-ffbb-456b-872c-a2f571c681cb of deviceClass UDP Input</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="61"/>
+        <source>UDP Output</source>
+        <extracomment>The name of the DeviceClass (31b00639-8904-4522-84ed-54c46a54c63c)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="64"/>
+        <source>IPv4 Address</source>
+        <extracomment>The name of the paramType (ea1be9fc-9a9b-44ba-a28d-e021aef4f046) of UDP Output</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="67"/>
+        <source>Send Data</source>
+        <extracomment>The name of the ActionType 6bc52462-b192-46a4-a6df-92cc5a479c89 of deviceClass UDP Output</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-guh-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="70"/>
+        <source>Data</source>
+        <extracomment>The name of the paramType (6604c852-6b24-4707-b8e5-1ddd8032efcc) of UDP Output</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
